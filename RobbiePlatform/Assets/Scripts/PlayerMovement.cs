a4d32572b45880779543a987f5f99aa21bbd8601
ShangdeHan <shangdehan@gmail.com>
2020-12-23 16:55:30 -0800

    Movement

---
+++ RobbiePlatform/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [Header("movement parameters")]
+    public float speed = 8f;
+    public float crouchSpeedDivisor = 3f;
+    float xVelocity;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        GroundMovement();
+    }
+
+    void GroundMovement()
+    {
+        xVelocity = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xVelocity * speed, rb.velocity.y);
+        FlipDirection();
+    }
+
+    void FlipDirection()
+    {
+        if(xVelocity < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        if (xVelocity > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+    }
+}
